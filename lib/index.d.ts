14b45fefeef42ca2c8cb32c362c1c7eb4760b273
Siddharth Joshi <sjoshi804@gmail.com>
2021-05-07 11:10:08 -0700

    stop tracking lib

--- lib/index.d.ts
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env node
-declare const chalk: any;
-declare const chalk_animation: any;
-declare const clear: any;
-declare const figlet: any;
-declare const path: any;
-declare const cli: any;
